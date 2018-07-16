635c4c2881e6a158da75f45d6b0e425aa0942643
Trevor F. Smith <trevor@trevor.smith.name>
2018-07-15 19:22:22 -0700

    Further the single page app by adding a media grid and populating it with image cards with images.

--- single-page-app/app.js
+++ single-page-app/app.js
@@ -2,25 +2,41 @@ import WebXRPolyfill from 'webxr-polyfill'
 
 import App from 'potassium-es/src/App'
 import Component from 'potassium-es/src/Component'
+import DataModel from 'potassium-es/src/DataModel'
+import DataCollection from 'potassium-es/src/DataCollection'
 
 import HeadingComponent from 'potassium-components/src/atoms/HeadingComponent.js'
-import MastheadComponent from 'potassium-components/src/organisms/MastheadComponent.js'
 
+import ImageCardComponent from 'potassium-components/src/molecules/ImageCardComponent.js'
+
+import MastheadComponent from 'potassium-components/src/organisms/MastheadComponent.js'
+import MediaGridComponent from 'potassium-components/src/organisms/MediaGridComponent.js'
+
+// Polyfill WebXR so that we can detect and use portal and immersive display modes
 new WebXRPolyfill()
 
 const SinglePageApp = class extends App {
 	constructor(){
 		super()
+		this._brand = 'PotassiumES'
+		this._titlePrefix = this._brand + ' • '
+
+		this._imageCollection = new ImageCollection()
 
 		this._masthead = new MastheadComponent(null, {
-			brand: 'PotassiumES'
+			brand: this._brand,
+			menuItems: [
+				{ name: 'Front', anchor: './#' },
+				{ name: 'About', anchor: './#about' },
+				{ name: 'Account', anchor: './#account' }
+			]
 		})
 		this.appendComponent(this._masthead)
 
 		// These are the views that we'll switch among when responding to Router events
 		this._viewsComponent = new Component().appendTo(this)
 		this._viewsComponent.addClass('views-component')
-		this._frontComponent = new FrontComponent().appendTo(this._viewsComponent)
+		this._frontComponent = new FrontComponent(this._imageCollection).appendTo(this._viewsComponent)
 		this._aboutComponent = new AboutComponent().appendTo(this._viewsComponent)
 		this._accountComponent = new AccountComponent().appendTo(this._viewsComponent)
 
@@ -30,24 +46,32 @@ const SinglePageApp = class extends App {
 		this.router.addRoute(/^account$/, 'account')
 		this.router.addListener(this._handleRoutes.bind(this))
 		this.router.start()
+
+		this._imageCollection.fetch()
 	}
 
 	showFront(){
 		this._frontComponent.removeClass('hidden')
 		this._aboutComponent.addClass('hidden')
 		this._accountComponent.addClass('hidden')
+		document.title = this._titlePrefix + 'Front'
+		this._masthead.navigationMenu.setSelected(0)
 	}
 
 	showAbout(){
 		this._frontComponent.addClass('hidden')
 		this._aboutComponent.removeClass('hidden')
 		this._accountComponent.addClass('hidden')
+		document.title = this._titlePrefix + 'About'
+		this._masthead.navigationMenu.setSelected(1)
 	}
 
 	showAccount(){
 		this._frontComponent.addClass('hidden')
 		this._aboutComponent.addClass('hidden')
 		this._accountComponent.removeClass('hidden')
+		document.title = this._titlePrefix + 'Account'
+		this._masthead.navigationMenu.setSelected(2)
 	}
 
 	_handleRoutes(routeName, hash, ...regexMatches){
@@ -69,14 +93,29 @@ document.addEventListener('DOMContentLoaded', ev => {
 	document.body.appendChild(window.app.el)
 })
 
+/**
+A couple of helper DataObjects for the media grid
+*/
+const ImageModel = class extends DataModel {}
+const ImageCollection = class extends DataCollection {
+	constructor(data=[], options={}){
+		super(data, Object.assign({
+			dataObject: ImageModel
+		}, options))
+	}
+	get url(){ return '/single-page-app/image-collection.json' }
+}
+
 const FrontComponent = class extends Component {
 	constructor(dataObject=null, options=null){
 		super(dataObject, options)
 		this.addClass('front-component')
-		this._headingComponent = new HeadingComponent(null, { text: 'Front' }).appendTo(this)
+
+		this._mediaGridComponent = new MediaGridComponent(this.dataObject, {
+			itemComponent: ImageCardComponent
+		}).appendTo(this)
 	}
 }
-
 
 const AboutComponent = class extends Component {
 	constructor(dataObject=null, options=null){
