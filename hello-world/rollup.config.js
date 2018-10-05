4b067f86a34011f5ec5a2d1cdeb0497dc194e51f
Trevor F. Smith <trevor@trevor.smith.name>
2018-10-04 22:47:55 -0700

    Moving over to KSS

--- hello-world/rollup.config.js
+++ hello-world/rollup.config.js
@@ -1,18 +1,20 @@
-import cleanup from 'rollup-plugin-cleanup';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import cleanup from 'rollup-plugin-cleanup'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: './hello-world/app.js',
-  output: {
-    file: './hello-world/build.js',
-    format: 'es'
-  },
-  plugins: [
-    commonjs(),
-    resolve(),
-    cleanup({
-      comments: 'none',
-    })
-  ]
-};
+	input: './hello-world/app.js',
+	output: {
+		file: './hello-world/build.js',
+		format: 'es'
+	},
+	plugins: [
+		commonjs(),
+		resolve(),
+		cleanup({
+			comments: 'none'
+		})
+	]
+}
--- scripts/hack-pull.sh
+++ scripts/hack-pull.sh
@@ -3,8 +3,13 @@
 # This tries to copy the relevant files from ../potassium-es/ and ../potassium-components/ into their respective node_modules
 # It's handy during development of the core platform
 
+cp -r ../postcss-potassium/*.js node_modules/postcss-potassium/
+cp -r ../postcss-potassium/src/*.js node_modules/postcss-potassium/src/
+
 cp -r ../potassium-es/src/* node_modules/potassium-es/src/
+cp -r ../potassium-es/styles/* node_modules/potassium-es/styles/
 cp -r ../potassium-es/static/* node_modules/potassium-es/static/
 
 cp -r ../potassium-components/src/* node_modules/potassium-components/src/
+cp -r ../potassium-components/styles/* node_modules/potassium-components/styles/
 cp -r ../potassium-components/static/* node_modules/potassium-components/static/
