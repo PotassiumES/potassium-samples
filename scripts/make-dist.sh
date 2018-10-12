cfd6ddb4217167534abc619a725524cba5b0862d
Trevor F. Smith <trevor@trevor.smith.name>
2018-10-11 20:54:07 -0700

    Update the script that collects files into an easily distributed directory.

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -3,7 +3,7 @@
 rm -rf ./dist/
 mkdir -p ./dist/
 cp -r ./static ./dist/
-cp index.html favicon.ico site.css site.js ./dist/
+cp index.html favicon.ico site.js ./dist/
 cp -r ./lib ./dist/
 cp -r ./hello-world ./dist/
 cp -r ./single-page-app ./dist/
