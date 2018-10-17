30bb53330386848f180d76947f16cd96e74e099d
Trevor F. Smith <trevor@trevor.smith.name>
2018-10-16 22:35:37 -0700

    Tweaked the splash page to somewhat match the PotassiumES.org page.

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -5,6 +5,6 @@ mkdir -p ./dist/
 cp -r ./static ./dist/
 cp index.html favicon.ico site.js ./dist/
 cp -r ./lib ./dist/
-cp -r ./grid ./dist/
+cp -r ./video-player ./dist/
 cp -r ./hello-world ./dist/
 cp -r ./single-page-app ./dist/
